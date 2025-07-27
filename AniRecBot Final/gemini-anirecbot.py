7b4004ec8e78ec646221606ad4c26de1e64e3e78
abdool <abduulrehman26@gmail.com>
2025-07-26 19:19:45 -0500

    Restructured the file structure and added a few lines in the .html and gemini-anirecbot.py file to help it work with flask. When running the gemini-anirecbot.py program, you can head to http://127.0.0.1:5000/ to access the chatbot

--- AniRecBot Final/gemini-anirecbot.py
+++ AniRecBot Final/gemini-anirecbot.py
@@ -274,7 +274,7 @@ def chat():
 if __name__ == "__main__":
     flask_app.run(debug=True)
 
-#print("Goodbye.")
+print("Goodbye.")
 
 
 #---------------------------------------Ignore Everything Below This----------------------------------------------------
